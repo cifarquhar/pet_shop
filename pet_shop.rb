a13b5fedeee140bb0c19e9c318163f5c39d3e910
Colin Farquhar <cifarquhar@gmail.com>
2017-02-03 10:51:05 +0000

    eighth test passing

--- pet_shop.rb
+++ pet_shop.rb
@@ -25,3 +25,12 @@ end
 def stock_count(shop)
   shop[:pets].length
 end
+
+def pets_by_breed(shop,breed)
+  breed_array = []
+  for pet in shop[:pets]
+    breed_array.push(pet) if pet.values.include?(breed) 
+  end
+  return breed_array
+end
+# This would push a pet into the array if it had a name which was also a breed, eg. a cat called "Husky". Better way of doing this?
